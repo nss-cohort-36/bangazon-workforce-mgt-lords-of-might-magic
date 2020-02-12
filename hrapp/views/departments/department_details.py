00695853abcd6a8ab658e86019d7853dc2cdcd4a
James Chapman <chapmanjamesg@gmail.com>
2020-02-12 15:31:55 -0600

    about to checkout adams pr

--- hrapp/views/departments/department_details.py
+++ hrapp/views/departments/department_details.py
@@ -6,28 +6,28 @@ from hrapp.models import Employee, Department
 from hrapp.models import model_factory
 from ..connection import Connection
 
-def create_department(cursor, row):
-    _row = sqlite3.Row(cursor, row)
+# def create_department(cursor, row):
+#     _row = sqlite3.Row(cursor, row)
 
-    department = Department()
-    department.id = _row['department_id']
-    department.name = _row['name']
-    department.budget = _row['budget']
+#     department = Department()
+#     department.id = _row['department_id']
+#     department.name = _row['name']
+#     department.budget = _row['budget']
 
-    employee = Employee()
-    employee.id = _row['id']
-    employee.first_name = _row['first_name']
-    employee.last_name = _row['last_name']
-    employee.department_id = _row['department_id']
+#     employee = Employee()
+#     employee.id = _row['id']
+#     employee.first_name = _row['first_name']
+#     employee.last_name = _row['last_name']
+#     employee.department_id = _row['department_id']
 
-    department.employee = []
+#     department.employee = []
 
-    return (department, employee,)
+#     return (department, employee,)
 
 
 def get_department(department_id):
     with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = create_department
+        conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
         db_cursor.execute("""
@@ -44,16 +44,39 @@ def get_department(department_id):
         where d.id = ?
         """, (department_id,))
 
-        return db_cursor.fetchone()
+        data =  db_cursor.fetchall()
 
-        department_employee = {}
+        department_check = []
+        employee_group = []
+        for row in data:
+            if(len(department_check) == 0):
+                department = Department()
+                department.id = row['department_id']
+                department.name = row['name']
+                department.budget = row['budget']
+                department_check.append(department)
 
-        for(department, employee) in departments:
-            if department.id not in department_employee:
-                department_employee[department.id] = department
-                department_employee[department.id].employees.append(employee)
-            else:
-                department_employee[department.id].employees.append(employee)
+            employee = Employee()
+            employee.id = row['id']
+            employee.first_name = row['first_name']
+            employee.last_name = row['last_name']
+            employee.department_id = row['department_id']
+            employee_group.append(employee)
+
+
+        department_obj = {}
+        
+        department_obj["department"] = department_check[0]
+        department_obj["employees"] = employee_group
+        return department_obj
+
+        # for(department, employee) in departments:
+        #     if department.id not in department_employee:
+        #         department_employee[department.id] = department
+        #         department_employee[department.id].employees.append(employee)
+        #     else:
+        #         department_employee[department.id].employees.append(employee)
+
 
 @login_required
 def department_details(request, department_id):
@@ -62,7 +85,8 @@ def department_details(request, department_id):
 
         template = 'departments/department_detail.html'
         context = {
-            'department': department
+            'department': department["department"],
+            'employees': department["employees"]
         }
 
         return render(request, template, context)
