1bd99a6b4d818f9eab59bdea717d9bd08f6da307
Manila Bui <manila.bui@gmail.com>
2020-02-12 10:26:12 -0600

    finished employee list

--- hrapp/models/department.py
+++ hrapp/models/department.py
@@ -3,4 +3,7 @@ from django.db import models
 class Department(models.Model):
     name = models.CharField(max_length=25)
     budget = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
     
