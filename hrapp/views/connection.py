fd2988b1e0bb843ed80f54d0ec8756292a0daee7
Chase Fite <chasef7031@gmail.com>
2020-02-12 14:07:18 -0600

    jenna

--- hrapp/views/connection.py
+++ hrapp/views/connection.py
@@ -1,2 +1,2 @@
 class Connection:
-    db_path = "/Users/ajbyr/workspace/python/bangazon-workforce-1/bangazon-workforce-mgt-lords-of-might-magic/db.sqlite3"
+    db_path = "/Users/Chase/workspace/python/bangazon-workforce-mgt-lords-of-might-magic/db.sqlite3"
