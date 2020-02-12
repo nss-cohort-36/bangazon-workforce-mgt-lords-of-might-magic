407e23185394bd80945b8fc45e52a8e72c8131eb
James Chapman <chapmanjamesg@gmail.com>
2020-02-12 10:59:47 -0600

    my stuff is working

--- hrapp/urls.py
+++ hrapp/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
     path('employees/', employee_list, name='employee_list'),
-    path('employees/form', employee_form, name='employee_form')
+    path('employees/form', employee_form, name='employee_form'),
     path('departments/', department_list, name='department_list')
 ]
