0de8f4be2f2f7b1758ae6224145d99dc14714549
Manila Bui <manila.bui@gmail.com>
2020-02-13 01:08:26 -0600

    finished

--- hrapp/views/employees/employee_details.py
+++ hrapp/views/employees/employee_details.py
@@ -12,11 +12,6 @@ def create_employee(cursor, row):
     e.first_name = _row["first_name"]
     e.last_name = _row["last_name"]
 
-    e.training_programs = []
-
-    t = TrainingProgram()
-    t.title = _row["training_program"]
-
     d = Department()
     d.name = _row["department"]
 
@@ -26,44 +21,70 @@ def create_employee(cursor, row):
     e.department = d
     e.computer = c
 
-    return (e, t,)
+    return e
+
+
+def create_training_program(cursor, row):
+    _row = sqlite3.Row(cursor, row)
+
+    t = TrainingProgram()
+    t.title = _row["title"]
+
+    return t
 
 
 def get_employee(employee_id):
     with sqlite3.connect(Connection.db_path) as conn:
         conn.row_factory = create_employee
-
         db_cursor = conn.cursor()
+
         db_cursor.execute("""
         SELECT
             e.id,
             e.first_name,
             e.last_name,
-            e.start_date,
-            e.is_supervisor,
             d.name department,
-            c.make computer,
-            t.title
+            c.make computer
         FROM
             hrapp_employee e
             JOIN hrapp_department d ON e.department_id = d.id
             LEFT JOIN hrapp_employeecomputer ec ON e.id = ec.employee_id
             LEFT JOIN hrapp_computer c ON c.id = ec.computer_id
-            LEFT JOIN hrapp_trainingprogramemployee te ON e.id = te.employee_id
-            LEFT JOIN hrapp_trainingprogram t ON t.id = te.training_program_id
         WHERE e.id = ?
         """, (employee_id,))
 
         return db_cursor.fetchone()
 
 
+def get_employee_training(employee_id):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = create_training_program
+
+        db_cursor = conn.cursor()
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            t.title
+        FROM
+            hrapp_employee e
+            LEFT JOIN hrapp_trainingprogramemployee te ON e.id = te.employee_id
+            LEFT JOIN hrapp_trainingprogram t ON t.id = te.training_program_id
+        WHERE
+            e.id = ?
+        """, (employee_id,))
+
+        return db_cursor.fetchall()
+
+
 def employee_details(request, employee_id):
     if request.method == 'GET':
         employee = get_employee(employee_id)
+        curr_employee_training = get_employee_training(employee_id)
 
-        template = 'employees/detail.html'
+        template = 'employees/employee_detail.html'
         context = {
-            'employee': employee
+            'employee': employee,
+            'training_programs': curr_employee_training
         }
 
         return render(request, template, context)
